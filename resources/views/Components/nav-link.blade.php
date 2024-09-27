5bc050503043147e7de94e63a765505d5b70d673
Imon <contact@imonweb.co.uk>
2024-09-27 23:45:47 +0100

    blade

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,1 +1,7 @@
-<a {{ $attributes }}>{{ $slot }}</a>
+{{-- <a {{ $attributes }}>{{ $slot }}</a> --}}
+
+@props(['active' => false])
+
+<a class="{{ $active ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}" aria-current="{{ $active ? 'page' : 'false' }}"
+  {{ $attributes }}
+  >{{ $slot }}</a>
