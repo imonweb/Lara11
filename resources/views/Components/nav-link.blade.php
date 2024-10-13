065d4dd8614c5337c794415661980c8faf0333bc
Imon <contact@imonweb.co.uk>
2024-10-13 22:48:11 +0100

    nav link

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,7 +1,17 @@
 {{-- <a {{ $attributes }}>{{ $slot }}</a> --}}
 
-{{-- @props(['active' => false]) --}}
+@props(['active' => false, 'type' => 'a'])
+
+<?php if ($type === 'a') : ?>
 
 <a class="{{ $active ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}" aria-current="{{ $active ? 'page' : 'false' }}"
   {{ $attributes }}
   >{{ $slot }}</a>
+
+<?php else : ?>
+
+<button class="{{ $active ? 'rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white'}}" aria-current="{{ $active ? 'page' : 'false' }}"
+  {{ $attributes }}
+  >{{ $slot }}</button>
+
+<?php endif ?>
