bb349061583316737523b43c6614235333a96f5f
Imon <contact@imonweb.co.uk>
2024-10-15 22:36:31 +0100

    view data and Route wildcards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,5 +3,8 @@
     Home Page
   </x-slot:heading>
   
-  <h1>{{ $greeting }}. from the Home Page. My name is {{ $name }}.</h1>
+  @foreach ($jobs as $job)
+    <li>{{ $job['title'] }}</li>
+
+  @endforeach
 </x-layout>
