7922ea5e1d2c3451c4516220ded18eb6b11ef8c4
Imon <contact@imonweb.co.uk>
2024-10-21 23:52:09 +0100

    view data and Route wildcards

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -4,13 +4,13 @@
   </x-slot:heading>
   
   <ul>
-    {{-- @foreach ($jobs as $job)
+    @foreach ($jobs as $job)
       <li>
         <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
           <strong>{{ $job['title'] }}:</strong> Pays {{ $job['salary'] }} per year.
         </a>
       </li>
 
-    @endforeach --}}
+    @endforeach
   </ul>
 </x-layout>
